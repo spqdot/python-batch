552899bc72e4aac4f1e204937e2d4a757ef6664b
spqdot <spqdot@gmail.com>
2025-05-28 18:24:24 +0100

    app.bat file added
    I have added this file

---
+++ app.bat
@@ -0,0 +1,5 @@
+CALL D:
+CALL cd "D:\Projects\GitHub\python-batch"
+CALL "C:\Users\Shrabani P\anaconda3\Scripts\activate.bat"
+CALL conda activate streamlit
+CALL streamlit run "D:\Projects\GitHub\python-batch\app.py" --server.port 8010
