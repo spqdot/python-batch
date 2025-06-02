5332578d369e08f729a6cb7b2c8f2451954c22c8
spqdot <spqdot@gmail.com>
2025-06-02 22:01:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Hello Shrabani - Yes Amit
 from sqlalchemy import create_engine, text
 import pandas as pd
+import datetime
 import streamlit as st
 
 st.set_page_config(layout="wide")
@@ -12,18 +13,20 @@ def get_connection(server):
     host = server['HOST']
     
     conn_string = f"""mssql+pyodbc://{username}:{password}@{host}/{database}?driver=ODBC+Driver+17+for+SQL+Server"""
-    print(conn_string)
     conn_engine=create_engine(conn_string)
     return conn_engine
 
-server_con_engine =  get_connection(st.secrets.SERVER01)
-
+# Page starts here
 st.title("Upload and Read Price Sample Data")
 st.write("This app allows you to upload CSV or Excel files and read data from SQL Server.")
 st.write("Please ensure that the uploaded file has the correct format and columns as required by the database.")
 
+server_con_engine =  get_connection(st.secrets.SERVER01)
+
 def save_data_to_server(df):
     with server_con_engine.begin() as conn:
+        df["TIMESTAMP"]=pd.to_datetime(df["TIMESTAMP"])
+        df["LastUpdated_date"] =datetime.datetime.now()
         df.to_sql(name='price_data_temp', con=conn, if_exists='replace', index=False)
 
         create_table = """
@@ -78,7 +81,7 @@ def save_data_to_server(df):
                 LastUpdated_date = source.LastUpdated_date
         WHEN NOT MATCHED THEN 
             INSERT (SYMBOL, SERIES, [OPEN], HIGH, LOW, [CLOSE], LAST, PREVCLOSE, TOTTRDQTY, TOTTRDVAL, TIMESTAMP, TOTALTRADES, ISIN, [TRANSACTION], LastUpdated_date)
-            VALUES (source.SYMBOL, source.SERIES, source.[OPEN], source.HIGH, source.LOW, source.[CLOSE], source.LAST, source.PREVCLOSE, source.TOTTRDQTY, source.TOTTRDVAL, source.TIMESTAMP, source.TOTALTRADES, source.ISIN, 'I', LastUpdated_date);
+            VALUES (source.SYMBOL, source.SERIES, source.[OPEN], source.HIGH, source.LOW, source.[CLOSE], source.LAST, source.PREVCLOSE, source.TOTTRDQTY, source.TOTTRDVAL, source.TIMESTAMP, source.TOTALTRADES, source.ISIN, 'I', source.LastUpdated_date);
         """)
 
         conn.execute(text(create_table))
@@ -101,9 +104,8 @@ with tab1:
             st.dataframe(df_upload.head())
             if st.button("Upload to SQL Server", key="upload_btn"):
                 with st.spinner('Uploading data to SQL Server...'):
-                    with server_con_engine.begin() as conn:
-                        # df_upload.to_sql(name='price_data', con=conn, if_exists='append', index=False)
-                        save_data_to_server(df_upload)
+                    # df_upload.to_sql(name='price_data', con=conn, if_exists='append', index=False)
+                    save_data_to_server(df_upload)
                     st.success("Data uploaded successfully to price_data table on sqlserver01.")
         except Exception as e:
             st.error(f"Error uploading file: {e}")
